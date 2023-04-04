bc3838655ed6ad5922630e444062b2c46e5b8b79
Mohsin Raza <mr.mohsinraza32@gmail.com>
2023-04-04 17:57:33 +0500

    Hello world Server completed

--- index.mjs
+++ index.mjs
@@ -1,11 +1,26 @@
 import express from 'express'
+import cors from 'cors';     // for cors issue
+
 const app = express()
-const port = 3000
+app.use(cors())
+// const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/home', (req, res) => {
+  res.send('I am home page!')
 })
+app.get('/profile', (req, res) => {
+  res.send('I am profile page!')
+})
+app.get('/weather', (req, res) => {
+  res.send({
+   weather:"30",
+   Temperature:"sunny",
+   city:"London"
+  })
+})
+ 
+const port = process.env.port || 3000;    // Heroku requirement
 
-app.listen(port,() => {
-    console.log(`Hello world server listening on port ${port}`);
+app.listen(3000,() => {
+    console.log(`Hello world server listening on port ${3000}`);
 })
